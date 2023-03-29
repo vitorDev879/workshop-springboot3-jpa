77859368f3edefbabab2d6621bf23df8aff85760
Vitor Hugo <vferreira879@yahoo.com>
2023-03-29 12:28:29 -0300

    User update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -14,22 +14,34 @@ import com.educandoweb.course.repositories.UserRepository;
 public class UserService {
 
 	@Autowired
-	private UserRepository repository; 
-	
+	private UserRepository repository;
+
 	public List<User> findAll() {
-		return repository.findAll(); 
+		return repository.findAll();
 	}
-	
-	public User findById(Long id) { 
-		Optional<User> obj =  repository.findById(id);
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
-	
-	public User insert(User obj) { 
-		return repository.save(obj); 
+
+	public User insert(User obj) {
+		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		repository.deleteById(id);
+	}
+
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 }
